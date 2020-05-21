debd634c32e057edcbc095a523baa89e3ff0c662
Trần Phan Phú Duy <tranphanphuduy2810@gmail.com>
2020-05-21 10:32:06 +0700

    [a] add bottom navigation

--- App.js
+++ App.js
@@ -1,25 +1,59 @@
+/* eslint-disable react/display-name */
+/* eslint-disable import/no-extraneous-dependencies */
 import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { StyleSheet, View, StatusBar, SafeAreaView } from 'react-native';
-import ForgotPassword from './src/components/authen/forgot-password';
-import SignUp from './src/components/authen/sign-up';
-import SignIn from './src/components/authen/sign-in';
-import SectionCourse from './src/components/home/SectionCourse';
+import {
+  StyleSheet, View, StatusBar, SafeAreaView,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import Home from './src/components/home';
-import ItemCategory from './src/components/browse/ItemCategory';
 import Browse from './src/components/browse';
-import CourseDetails from './src/components/course-detail/index';
-import Module from './src/components/course-detail/ItemContent';
-import AuthorProfile from './src/components/author-profile';
 import Search from './src/components/search';
+import screenName from './src/constants/screen-name';
+import colorSource from './src/constants/color';
 
+const HomeScreen = () => (<Home/>);
+const DownloadScreen = () => (<View/>);
+const BrowseScreen = () => (<Browse/>);
+const SearchScreen = () => (<Search/>);
 
+const Tab = createBottomTabNavigator();
 function App() {
   return (
+    <NavigationContainer>
       <SafeAreaView style={styles.container}>
         <StatusBar backgroundColor="#000" barStyle="light-content" />
-        <Search />
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === screenName.HomeScreen) {
+                iconName = 'home';
+              } else if (route.name === screenName.DownloadScreen) {
+                iconName = 'clouddownloado';
+              } else if (route.name === screenName.BrowseScreen) {
+                iconName = 'find';
+              } else {
+                iconName = 'search1';
+              }
+              // You can return any component that you like here!
+              return <AntDesign name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: colorSource.purple,
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen name={screenName.HomeScreen} component={HomeScreen}/>
+          <Tab.Screen name={screenName.DownloadScreen} component={DownloadScreen}/>
+          <Tab.Screen name={screenName.BrowseScreen} component={BrowseScreen}/>
+          <Tab.Screen name={screenName.SearchScreen} component={SearchScreen}/>
+        </Tab.Navigator>
       </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
---
+++ src/constants/screen-name.js
@@ -0,0 +1,8 @@
+const screenName = {
+  HomeScreen: 'Home',
+  DownloadScreen: 'Downloads',
+  BrowseScreen: 'Browsw',
+  SearchScreen: 'Search',
+};
+
+export default screenName;
